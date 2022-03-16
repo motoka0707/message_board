f4e3b6607a5cf8e6f3aa055c07bdd1152fa20ff3
motoka_01 <motoka_01@192.168.3.16>
2022-03-16 17:49:00 +0900

    フラッシュメッセージを追加

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -12,19 +11,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
 import utils.DBUtil;
+
 /**
- * Servlet implementation class UpdateServle
+ * Servlet implementation class DestroyServlet
  */
-@WebServlet("/update")
-public class UpdateServle extends HttpServlet {
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateServle() {
+    public DestroyServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
@@ -39,20 +38,10 @@ public class UpdateServle extends HttpServlet {
             // 該当のIDのメッセージ1件のみをデータベースから取得
             Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
-            // フォームの内容を各フィールドに上書き
-            String title = request.getParameter("title");
-            m.setTitle(title);
-
-            String content = request.getParameter("content");
-            m.setContent(content);
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
-
-            // データベースを更新
             em.getTransaction().begin();
+            em.remove(m);       // データ削除
             em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "更新が完了しました。");
+            request.getSession().setAttribute("flush", "削除が完了しました。");
             em.close();
 
             // セッションスコープ上の不要になったデータを削除
@@ -60,8 +49,6 @@ public class UpdateServle extends HttpServlet {
 
             // indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
-
+        }
     }
-
-}
 }
